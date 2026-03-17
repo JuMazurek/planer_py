bbd360b3b8ee46ca209292f2a670bf26c20ddb0e
JuMazurek <justyna.mazurek@protonmail.com>
2026-03-17 16:37:09 +0100

    Update Planner.py
    Translated from Polish to English :)

--- Planner.py
+++ Planner.py
@@ -1,276 +1,267 @@
-"""
-jeden wpis to słownik; słownik zawiera pary
-
-wszystkie wpisy to lista
-"""
-#(osobna funkcja na każdą akcję) - wybór za pomocą menu nawigacji.
-
 from datetime import datetime
 import json
 import os
 
-priorytet_waga = {
-    "niski": 1,
-    "średni": 2,
-    "wysoki": 3
+priority_weight = {
+    "low": 1,
+    "average": 2,
+    "high": 3
 }
 
-lista_wpisow = []
+entry_list = []
 
 
-def pokaz_menu():
-    print("PLAN DNIA")
-    print("1. Wczytaj z pliku")
-    print("2. Nowy wpis")
-    print("3. Wyświetl wpisy")
-    print("4. Edytuj wpis")
-    print("5. Zapisz do pliku")
-    print("0. Zakończ program")
+def show_menu():
+    print("PLANNER")
+    print("1. Load file")
+    print("2. New entry")
+    print("3. Show entries")
+    print("4. Edit entry")
+    print("5. Save to file")
+    print("0. Close the program")
+#creating the entry
+def create_entry():
+    print("CREATING NEW ENTRY")
+    #subject / "What?"
+    while True: 
 
-def utworz_wpis():
-    print("DODAWANIE NOWEGO WPISU")
-
-    while True: #temat
-
-        temat = input("Temat *: ").strip() #usuwanie pustych znaków, żeby spacja albo enter nie mogło być tytułem wpisu
-        if temat: #sprawdzenie czy puste
+        subject = input("Subject *: ").strip() #removing blank characters, so space or enter won't be accepted as entry subject
+        if subject: #check if empty/blank
             break
         else:
-            print("To pole nie może być puste. Spróbuj ponownie.")
+            print("This section can't be empty. Try again.")
+    #date and time / "When?"
+    while True: 
 
-    while True: #czas
-
-        czas = input("Godzina i data (DD.MM.RRRR HH:MM) *: ").strip()
+        date_time = input("Time and date (DD.MM.YYYY HH:MM) *: ").strip()
         try:
-            dt = datetime.strptime(czas, "%d.%m.%Y %H:%M")
-            czas = dt.strftime("%d.%m.%Y %H:%M")
+            dt = datetime.strptime(date_time, "%d.%m.%Y %H:%M")
+            date_time = dt.strftime("%d.%m.%Y %H:%M")
             break
         except ValueError:
-            print("Nieprawidłowy format. Użyj DD.MM.RRR HH:MM.")
-    
-    miejsce = input("Miejsce: ").strip() #miejsce
-
-    while True: #priorytet
-        priorytet = input("Jaki priorytet ma ten wpis? Wybierz niski, średni lub wysoki: ").strip().lower()
-        if priorytet == "":
-            priorytet = "niski"
+            print("Wrong input. Use DD.MM.YYYY HH:MM.")
+    #localisation / "Where?"
+    localisation = input("Localisation: ").strip() 
+    #priority / "Is important?"
+    while True: 
+        priority = input("What priority does this event have? Choose low, average or high: ").strip().lower()
+        if priority == "":
+            priority = "low"
             break
-        elif priorytet in ["niski", "średni", "wysoki"]:
-            break
-        else:
-            print("Niepoprawny wybór. Wybierz niski, średni lub wysoki.")
-
-    while True: #notatki
-        notatki = input("Notatki (maks. 256 znaków): ").strip()
-        if len(notatki) <= 256:
+        elif priority in ["low", "average", "high"]:
             break
         else:
-            print("Limit znaków to 256. Wprowadź notatkę ponownie.")
+            print("Invalid choice. Choose low, average or high.")
+    #description / "What is this about?"
+    while True: 
+        description = input("Description (max. 256 characters): ").strip()
+        if len(description) <= 256:
+            break
+        else:
+            print("Maximum length exceeded. Please re-enter the description.")
 
-    nowy_wpis = {
-        "temat": temat,
-        "czas": czas,
-        "miejsce": miejsce,
-        "priorytet": priorytet,
-        "notatki": notatki
+    new_entry = {
+        "subject": subject,
+        "date_time": date_time,
+        "localisation": localisation,
+        "priority": priority,
+        "description": description
     }
 
-    lista_wpisow.append(nowy_wpis)
-    print("Dodano wpis.")
+    entry_list.append(new_entry)
+    print("New entry added.")
 
-
-def wyswietl_wpisy(lista_wpisow):
-    global priorytet_waga
-    if not lista_wpisow: 
-        print("Brak wpisów do wyświetlenia.")
+#viewing existing entries
+def view_entries(entry_list):
+    global priority_weight
+    if not entry_list: 
+        print("No entries found.")
         return
-    print("\nSortować wpisy?\n1. W kolejności utworzenia (domyślnie)\n2. Po dacie i godzinie (od najstarszego) \n3. Po dacie i godzinie (od najnowszego)\n4. Wg priorytetu wpisu")
-    wybor_sortowania = input("Wybór: ").strip()
+    print("\nSort entries\n1. First created (default)\n2. Date and time (by oldest) \n3. Date and time (by newest)\n4. Priority")
+    sort_type = input("Select: ").strip()
 
-    if wybor_sortowania == "2":
-        lista_wpisow.sort(key=lambda wpis: datetime.strptime(wpis["czas"], "%d.%m.%Y %H:%M"))
-    elif wybor_sortowania == "3":
-        lista_wpisow.sort(reverse=True, key=lambda wpis: datetime.strptime(wpis["czas"], "%d.%m.%Y %H:%M"))
-    elif wybor_sortowania == "4":
-        lista_wpisow.sort(reverse=True, key=lambda wpis: priorytet_waga.get(wpis["priorytet"], 0))
+    if sort_type == "2":
+        entry_list.sort(key=lambda entry: datetime.strptime(entry["time"], "%d.%m.%Y %H:%M"))
+    elif sort_type == "3":
+        entry_list.sort(reverse=True, key=lambda entry: datetime.strptime(entry["time"], "%d.%m.%Y %H:%M"))
+    elif sort_type == "4":
+        entry_list.sort(reverse=True, key=lambda entry: priority_weight.get(entry["priority"], 0))
     else:
-        print("LISTA WPISÓW")
-    for indeks_wpisu, wpis in enumerate(lista_wpisow, start=1):
-        print(f"\nWpis #{indeks_wpisu}:")
-        print(f"Temat: {wpis['temat']}")
-        print(f"Czas: {wpis['czas']}")
-        print(f"Miejsce: {wpis['miejsce']}")
-        print(f"Priorytet: {wpis['priorytet']}")
-        print(f"Notatki: {wpis['notatki']}")
-    #zostaną wyświetlone wszystkie zadania z pamięci programu, dla chętnych - sortowanie wg daty i godziny
-
-def edytuj_wpis(lista_wpisow):
-    if not lista_wpisow:
-        print("Brak wpisów do edycji.")
+        print("ENTRIES")
+    for entry_index, entry in enumerate(entry_list, start=1):
+        print(f"\nEntry #{entry_index}:")
+        print(f"Subject: {entry['subject']}")
+        print(f"Date and time: {entry['date_time']}")
+        print(f"Localisation: {entry['localisation']}")
+        print(f"Priority: {entry['priority']}")
+        print(f"Description: {entry['description']}")
+    
+#editing entries
+def edit_entry(entry_list):
+    if not entry_list:
+        print("No entries available.")
         return
 
-    print("EDYCJA WPISU")
-    for indeks_wpisu, wpis in enumerate(lista_wpisow, start=1):
-        print(f"{indeks_wpisu}. {wpis['temat']} ({wpis['czas']})")
+    print("EDIT ENTRY")
+    for entry_index, entry in enumerate(entry_list, start=1):
+        print(f"{entry_index}. {entry['subject']} ({entry['date_time']})")
 
     while True:
         try:
-            wybor = int(input("Wybierz numer wpisu do edycji: "))
-            if 1 <= wybor <= len(lista_wpisow):
-                wybrany_wpis = lista_wpisow[wybor-1]
+            choice = int(input("Select which entry to edit: "))
+            if 1 <= choice <= len(entry_list):
+                selected_entry = entry_list[choice-1]
                 break
             else:
-                print("Nieprawidłowy numer.")
+                print("Invalid number.")
         except ValueError:
-            print("Podaj liczbę.")
+            print("Enter number.")
 
-    print("\nWybrany wpis:")
-    for klucz, wartosc in wybrany_wpis.items():
-        print(f"{klucz.capitalize()}: {wartosc}")
+    print("\nSelected entry:")
+    for entry_key, value in selected_entry.items():
+        print(f"{entry_key.capitalize()}: {value}")
 
-    decyzja = input("Chcesz zmodyfikować czy usunąć wpis? Wpisz edytuj lub usuń: ").strip().lower()
-    if decyzja == "usuń":
-        potwierdzenie = input("Na pewno chcesz usunąć wpis?: ").strip().lower()
-        if potwierdzenie == "tak":
-            lista_wpisow.pop(wybor-1)
-            print("Wpis usunięty.")
+    decision = input("Do you want to edit or delete the entry? Type edit or delete: ").strip().lower()
+    if decision == "delete":
+        confirm = input("Are you sure? ").strip().lower()
+        if confirm == "yes":
+            entry_list.pop(choice-1)
+            print("Entry deleted.")
         else:
-            print("Operacja anulowana.")
-    elif decyzja == "edytuj":
-        # Formularz edycji
-        print("Wciśnij Enter, aby pozostawić aktualną wartość.")
-        temat = input(f"Temat [{wybrany_wpis['temat']}]: ").strip()
-        if temat:
-            wybrany_wpis['temat'] = temat
+            print("Operation cancelled.")
+    elif decision == "edit":
+        # edit form
+        print("Press Enter to leave the current contents.")
+        subject = input(f"Subject [{selected_entry['subject']}]: ").strip()
+        if subject:
+            selected_entry['subject'] = subject
 
         while True:
-            czas = input(f"Czas [{wybrany_wpis['czas']}] (DD.MM.RRRR HH:MM): ").strip()
-            if not czas:
+            date_time = input(f"Date and time [{selected_entry['time']}] (DD.MM.YYYY HH:MM): ").strip()
+            if not date_time:
                 break
             try:
-                dt = datetime.strptime(czas, "%d.%m.%Y %H:%M")
-                wybrany_wpis['czas'] = dt.strftime("%d.%m.%Y %H:%M")
+                dt = datetime.strptime(date_time, "%d.%m.%Y %H:%M")
+                selected_entry['time'] = dt.strftime("%d.%m.%Y %H:%M")
                 break
             except ValueError:
-                print("Nieprawidłowy format.")
+                print("Invalid input.")
 
-        miejsce = input(f"Miejsce [{wybrany_wpis['miejsce']}]: ").strip()
-        if miejsce:
-            wybrany_wpis['miejsce'] = miejsce
-
-        while True:
-            priorytet = input(f"Priorytet [{wybrany_wpis['priorytet']}] (niski/średni/wysoki): ").strip().lower()
-            if not priorytet:
-                break
-            if priorytet in ["niski", "średni", "wysoki"]:
-                wybrany_wpis['priorytet'] = priorytet
-                break
-            else:
-                print("Niepoprawny wybór.")
+        localisation = input(f"localisation [{selected_entry['localisation']}]: ").strip()
+        if localisation:
+            selected_entry['localisation'] = localisation
 
         while True:
-            notatki = input(f"Notatki [{wybrany_wpis['notatki']}]: ").strip()
-            if not notatki:
+            priority = input(f"priority [{selected_entry['priority']}] (low/average/high): ").strip().lower()
+            if not priority:
                 break
-            if len(notatki) <= 256:
-                wybrany_wpis['notatki'] = notatki
+            if priority in ["low", "average", "high"]:
+                selected_entry['priority'] = priority
                 break
             else:
-                print("Limit znaków 256.")
+                print("Invalid input.")
 
-        print("Wpis zaktualizowany.")
+        while True:
+            description = input(f"description [{selected_entry['description']}]: ").strip()
+            if not description:
+                break
+            if len(description) <= 256:
+                selected_entry['description'] = description
+                break
+            else:
+                print("Max. lenght: 256 char.")
+
+        print("Entry updated.")
     else:
-        print("Niepoprawna opcja. Powrót do menu.")
+        print("Invalid input. Returning to menu")
 
-plik_wyjsciowy = "plan_dnia.json"
+#saving to file
+save_file = "day_plan.json"
+def save_to_file(entry_list):
 
-def zapisz_do_pliku(lista_wpisow):
-
-    print("ZAPIS DO PLIKU")
-    wybor_pliku = input(f"Zapisać do domyślnego pliku '{plik_wyjsciowy}'? ").strip().lower()
-    if wybor_pliku == "nie":
-        nazwa_pliku = input("Podaj nazwę pliku (użyj rozszerzenia .json): ").strip()
-        if not nazwa_pliku.endswith(".json"):
-            nazwa_pliku += ".json"
+    print("SAVING TO FILE")
+    select_file = input(f"Save to default file '{save_file}'? ").strip().lower()
+    if select_file == "No":
+        file_name = input("Type file name: ").strip()
+        if not file_name.endswith(".json"):
+            file_name += ".json"
     else:
-        nazwa_pliku = plik_wyjsciowy
+        file_name = save_file
 
     try:
-        with open(nazwa_pliku,"w", encoding="utf-8") as zapisz:
-            json.dump(lista_wpisow, zapisz, ensure_ascii=False, indent=4)
-        print(f"Dane zapisano do pliku '{nazwa_pliku}'.")
-    except Exception as blad:
-        print(f"Wystąpił błąd podczas zapisu: {blad}")
+        with open(file_name,"w", encoding="utf-8") as save:
+            json.dump(entry_list, save, ensure_ascii=False, indent=4)
+        print(f"Saved as '{file_name}'.")
+    except Exception as error:
+        print(f"An error occured while trying to save: {error}")
    
-
-def wczytaj_z_pliku():
-    print("WCZYTYWANIE Z PLIKU")
-    pliki = [plik for plik in os.listdir() if plik.endswith(".json")]
-    if not pliki:
-        print("Brak plików JSON w tej lokalizacji.")
+#reading the file
+def read_file():
+    print("READING THE FILE")
+    files = [file for file in os.listdir() if file.endswith(".json")]
+    if not files:
+        print("No available files with .json extention in this localisation.")
         return None
     
-    print("Dostępne pliki: ")
-    for indeks_pliku, plik in enumerate(pliki, start=1):
-        print(f"{indeks_pliku}.{plik}")
+    print("Available files: ")
+    for file_index, file in enumerate(files, start=1):
+        print(f"{file_index}.{file}")
 
     while True:
-            wybor_pliku = input("Wybierz numer pliku do wczytania lub wpisz " + "Anuluj" + " aby anulować: ").strip().lower()
+            select_file = input("Select file to upload or type " + "Cancel" + " to cancel: ").strip().lower()
 
-            if wybor_pliku == "anuluj":
-                print("Wczytywanie anulowane.")
+            if select_file == "cancel":
+                print("Uploading cancelled.")
                 return None
             try:
-                wybor_pliku = int(wybor_pliku)
-                if 1 <= wybor_pliku <= len(pliki):
-                    wybrany_plik = pliki[wybor_pliku-1]
+                select_file = int(select_file)
+                if 1 <= select_file <= len(files):
+                    selected_file = files[select_file-1]
                     break
                 else:
-                    print("Niepoprawny numer. Wybierz ponownie.")
+                    print("Invalid input. Try again.")
             except ValueError:
-                print("Nieprawidłowy wybór. Podaj liczbę.")
+                print("Invalid input. Type a number.")
 
     try:
-        with open(wybrany_plik, "r", encoding="utf-8") as plik:
-            lista = json.load(plik)
-        print(f"Wczytano dane z pliku '{wybrany_plik}'.")
-        return lista
-    except Exception as blad:
-        print(f"Wystąpił błąd podczas wczytywania pliku: {blad}")
+        with open(selected_file, "r", encoding="utf-8") as file:
+            list = json.load(file)
+        print(f"Uploaded the data from '{selected_file}'.")
+        return list
+    except Exception as error:
+        print(f"An error occured while trying to read the file: {error}")
         return None
 
 
-    
-
 while True:
-    pokaz_menu()
+    show_menu()
 
     try:
-        wybor = int(input("Wybierz opcję: "))
+        choice = int(input("Select option:  "))
     except ValueError:
-        print("Spróbuj ponownie. Wybierz jedną z podanych opcji.")
+        print("Try again. Select from options below: ")
         continue
 
-    if wybor == 1:
-        nowe_wpisy = wczytaj_z_pliku()
-        if nowe_wpisy is not None:
-            lista_wpisow = nowe_wpisy
+    if choice == 1:
+        new_entries = read_file()
+        if new_entries is not None:
+            entry_list = new_entries
         else:
-            print("Pozostawiono poprzednie wpisy w pamięci.")
-    elif wybor == 2:
-        utworz_wpis()
-    elif wybor == 3:
-        wyswietl_wpisy(lista_wpisow)
-    elif wybor == 4:
-        edytuj_wpis(lista_wpisow)
-    elif wybor == 5:
-        zapisz_do_pliku(lista_wpisow)
-    elif wybor == 0:
-        czy_zapisac = input("Czy chcesz zapisać do pliku przed zakończeniem programu? ").strip().lower()
-        if czy_zapisac == "tak":
-            zapisz_do_pliku(lista_wpisow)
+            print("The previous entries were not cleared.")
+    elif choice == 2:
+        create_entry()
+    elif choice == 3:
+        view_entries(entry_list)
+    elif choice == 4:
+        edit_entry(entry_list)
+    elif choice == 5:
+        save_to_file(entry_list)
+    elif choice == 0:
+        if_save = input("Do you want to save before closing the program?").strip().lower()
+        if if_save == "yes":
+            save_to_file(entry_list)
         else:
-            print("Do zobaczenia!")
+            print("See you soon!")
             break
     else:
-        print("Nieprawidłowy wybór, spróbuj ponownie.")
+        print("Invalid input, try again.")
